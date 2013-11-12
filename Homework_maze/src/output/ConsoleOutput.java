61e3ed29fdac981c6aa56ed1dcaf113d9ab8edd6
asto <astomusic@naver.com>
2013-11-12 13:47:35 +0900

    131112 update 	결과 출력물 수정 (ENTER키로 넘어가게끔)

--- Homework_maze/src/output/ConsoleOutput.java
+++ Homework_maze/src/output/ConsoleOutput.java
@@ -1,18 +1,23 @@
 package output;
 
+import java.util.Scanner;
+
 import map.Map;
 
 public class ConsoleOutput extends GenerateOutPut {
+	Scanner scanner = new Scanner(System.in);
 
 	public static final String NEWLINE = System.getProperty("line.separator");
 	
-	public void show(Map map) {
+	public void show(Map map){
 		for (int i=0 ; i <= map.mapSizeX ; i++) {
 			for(int j=0 ; j <= map.mapSizeY ; j++) {
 				System.out.print(map.getMap()[i][j] + " ");
 			}
 			System.out.println();
 		}
+		System.out.println("press ENTER key...");
+		scanner.nextLine();
 	}
 	
 	public void show(String str) {
